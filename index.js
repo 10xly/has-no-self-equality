e7cd8a47ec1a08497fac06c430e8e57d5c1648f3
tj-commits <thefoxjones@gmail.com>
2025-12-17 09:40:36 -0600

    Sigma

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-require('vanilla-javascript') // because we are making something awesome!
-require('none')() // for performance
-
 const hasSelfEquality = require('has-self-equality')
-const not = require('not')
 
-function hasNoSelfEquality(value) {
-  return not(() => hasSelfEquality(value))()
+module.exports = function hasNoSelfEquality(value) {
+  return !hasSelfEquality(value)
 }
-
-module.exports = hasNoSelfEquality
